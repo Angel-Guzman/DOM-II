a0d4e11267de92c317b5d4ec534769ca77b2406b
Angelg10 <angelguzman97@gmail.com>
2020-04-07 16:04:39 -0400

    click, keydown, and resize used

--- js/index.js
+++ js/index.js
@@ -10,3 +10,42 @@ function navLinksHover() {
 }
 navLinksHover()
 
+// NOT USED: wheel, drag/drop, load, focus, scroll, select, dblclick
+
+// USED: mouseenter, mouseleave, click, keydown, resize
+
+function btnClickColor() {
+    const buttons = document.querySelectorAll('div.btn')[0];
+        buttons.addEventListener('click', (event) => {
+        event.target.style.backgroundColor = 'purple';
+        })
+        buttons.addEventListener('mouseleave', (event) => {
+            event.target.style.backgroundColor = '#17A2B8';    
+        });
+}
+btnClickColor()
+
+function btnAlert() {
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            alert('YOU CANT ESCAPE!')
+        }
+    })
+}
+btnAlert()
+
+function footerResize(event) {
+    event.preventDefault();
+
+    scale += event.deltaY * -0.01;
+
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    el.style.transform = `scale(${scale})`;
+    
+}
+
+let scale = 1;
+const el = document.querySelector('.footer');
+el.onwheel = footerResize;
+
