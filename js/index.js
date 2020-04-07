ca6fb09679e20534f139a70709eac8240febb64d
Angelg10 <angelguzman97@gmail.com>
2020-04-07 11:46:40 -0400

    added mouseenter/mouseleave for first nav link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+function navLinksHover() {
+    const navLink = document.querySelectorAll('a.nav-link')[0];
+        navLink.addEventListener('mouseenter', (event) => {
+        event.target.style.backgroundColor = 'gray';
+        });
+        navLink.addEventListener('mouseleave', (event) => {
+        event.target.style.backgroundColor = 'white';    
+        });
+}
+navLinksHover()
+
